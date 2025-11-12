750be5afa862f2999ef298dbcd7eaa5dba68d7c5
offyerrocker <offyerrocker88@gmail.com>
2025-11-11 22:18:02 -0800

    reenable 3p anim callbacks
    re-enabled animation callbacks from the third-person unit execution animation
    i don't know why this works now and not before.

--- lua/huskplayermovement.lua
+++ lua/huskplayermovement.lua
@@ -30,15 +30,12 @@ end
 function HuskPlayerMovement:anim_stop_execution(...)
 --	Print("HuskPlayerMovement:anim_stop_execution()")
 --	managers.player:local_player():movement():change_state("standard")
-
---[[
 	local player = managers.player:local_player()
 	local mov_ext = alive(player) and player:movement()
 	local state = mov_ext and mov_ext:current_state()
 	if state then 
 		mov_ext:change_state("standard")
 	end
-	--]]
 end
 
 --]]
